a5de3beaba2bc51c6c1708ef2ba5d7481992e804
Cody Nicholson <codynichoson96@gmail.com>
2021-01-17 17:03:55 -0600

    Working on db

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,2 @@
+CREATE TABLE users (user_id uuid PRIMARY KEY, name VARCHAR(255) NOT NULL, email VARCHAR(255));
+CREATE TABLE organization (org_id uuid PRIMARY KEY, name varchar(255), address varchar(255));
