63273b4bceec3b3031455cacf5d34a81cf14b340
Pedro Rebollar <prebolla15@gmail.com>
2021-05-31 21:43:13 -0500

    removed list for getUser method in profile dao

--- src/main/java/com/cag/cagbackendapi/daos/impl/ProfileDao.kt
+++ src/main/java/com/cag/cagbackendapi/daos/impl/ProfileDao.kt
@@ -60,31 +60,37 @@ class ProfileDao : ProfileDaoI{
         return savedProfileEntity.toDto()
     }
 
-    override fun getUserWithProfile(userId: UUID): ProfileDto? {
+    /*override fun getUserWithProfile(userId: UUID): ProfileDto? {
         return if(profileRepository.getByUserEntity_userId(userId).isEmpty()){
             null
         }else{
             profileRepository.getByUserEntity_userId(userId)[0].toDto()
         }
+    }*/
+
+    override fun getUserWithProfile(userId: UUID): ProfileDto? {
+        return if(profileRepository.getByUserEntity_userId(userId) == null){
+            null
+        }else{
+            profileRepository.getByUserEntity_userId(userId).toDto()
+        }
     }
+        //return profileRepository.getByUserEntity_userId(userId).toDto() ?: null
 
-//    override fun getUserWithProfile(userId: UUID): ProfileDto? {
-//        return profileRepository.getByUserEntity_userId(userId).toDto() ?: null
-//    }
-
-//    override fun getProfile(userId: UUID): ProfileDto? {
-//        logger.info(GET_PROFILE(userId))
-//
-//        val profileEntity = profileRepository.getByUserEntity_userId(userId) ?: return null
-//        return profileEntity.toDto()
-//    }
 
     override fun getProfile(userId: UUID): ProfileDto? {
         logger.info(GET_PROFILE(userId))
 
-        val profileEntity = profileRepository.getByUserEntity_userId(userId)[0] ?: return null
+        val profileEntity = profileRepository.getByUserEntity_userId(userId) ?: return null
         return profileEntity.toDto()
     }
+
+/*    override fun getProfile(userId: UUID): ProfileDto? {
+        logger.info(GET_PROFILE(userId))
+
+        val profileEntity = profileRepository.getByUserEntity_userId(userId)[0] ?: return null
+        return profileEntity.toDto()
+    }*/
 
     private fun profileDtoToEntity(profileDto: ProfileDto): ProfileEntity {
         return modelMapper.map(profileDto, ProfileEntity::class.java)
--- src/main/java/com/cag/cagbackendapi/repositories/ProfileRepository.java
+++ src/main/java/com/cag/cagbackendapi/repositories/ProfileRepository.java
@@ -2,20 +2,20 @@ package com.cag.cagbackendapi.repositories;
 
 import com.cag.cagbackendapi.entities.ProfileEntity;
 import com.cag.cagbackendapi.entities.UserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
-@Repository
+/*@Repository
 public interface ProfileRepository extends CrudRepository<ProfileEntity, UUID> {
     List<ProfileEntity> getByUserEntity_userId(UUID userId);
+}*/
 
 
+@Repository
+public interface ProfileRepository extends CrudRepository<ProfileEntity, UUID> {
+    ProfileEntity getByUserEntity_userId(UUID userId);
 }
-
-//@Repository
-//public interface ProfileRepository extends CrudRepository<ProfileEntity, UUID> {
-//    ProfileEntity getByUserEntity_userId(UUID userId);
-//}
