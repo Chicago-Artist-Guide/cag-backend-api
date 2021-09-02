2a25c750bc8b1a46571361cf291490e2e3135126
Pedro Rebollar <prebolla15@gmail.com>
2021-09-01 22:56:16 -0500

    updated the sql scripts with cascade deletes

---
+++ src/main/resources/db/migration/V20210901224849__add_union_status_and_age_values.sql
@@ -0,0 +1,12 @@
+INSERT INTO union_status(name) VALUES ('United Actors of America');
+INSERT INTO union_status(name) VALUES ('National Actors Union');
+INSERT INTO age_increment(ages) VALUES ('18-22');
+INSERT INTO age_increment(ages) VALUES ('23-27');
+INSERT INTO age_increment(ages) VALUES ('28-32');
+INSERT INTO age_increment(ages) VALUES ('33-37');
+INSERT INTO age_increment(ages) VALUES ('38-42');
+INSERT INTO age_increment(ages) VALUES ('43-47');
+INSERT INTO age_increment(ages) VALUES ('48-52');
+INSERT INTO age_increment(ages) VALUES ('53-57');
+INSERT INTO age_increment(ages) VALUES ('58-62');
+INSERT INTO age_increment(ages) VALUES ('62+');
