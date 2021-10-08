53d8279723a12111990e0ff74e273a50d4e5ac6e
Pedro Rebollar <prebolla15@gmail.com>
2021-10-08 17:39:49 -0500

    added integration test for extra profile info and checking award info

--- src/test/java/com/cag/cagbackendapi/integration/ProfileControllerIntegrationTests.kt
+++ src/test/java/com/cag/cagbackendapi/integration/ProfileControllerIntegrationTests.kt
@@ -2,10 +2,8 @@ package com.cag.cagbackendapi.integration
 
 import com.cag.cagbackendapi.constants.DetailedErrorMessages
 import com.cag.cagbackendapi.constants.RestErrorMessages
-import com.cag.cagbackendapi.dtos.ProfileDto
-import com.cag.cagbackendapi.dtos.ProfileRegistrationDto
-import com.cag.cagbackendapi.dtos.UserDto
-import com.cag.cagbackendapi.dtos.UserRegistrationDto
+import com.cag.cagbackendapi.dtos.*
+import com.cag.cagbackendapi.entities.AwardRegistrationEntity
 import com.cag.cagbackendapi.errors.ErrorDetails
 import com.cag.cagbackendapi.util.SpringCommandLineProfileResolver
 import com.cag.cagbackendapi.util.TestDataCreatorService
@@ -21,6 +19,7 @@ import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
 import org.springframework.test.context.ActiveProfiles
+import java.util.*
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles(value = ["LOCAL"], resolver = SpringCommandLineProfileResolver::class)
@@ -37,7 +36,7 @@ class ProfileControllerIntegrationTests {
 
     private val userProfileInvalidUnionStatus = ProfileRegistrationDto(pronouns = "he/him", lgbtqplus_member = false, gender_identity = "male", comfortable_playing_transition = true, comfortable_playing_man = true, comfortable_playing_women = true, comfortable_playing_neither = false, height_inches = 88, agency = "Pedro LLC", website_link_one = "", website_link_two = "", website_type_one = "", website_type_two = "", bio = "this is my bio", landing_perform_type_on_stage = true, landing_perform_type_off_stage = false, actor_info_1_ethnicities = listOf("Hispanic"), actor_info_2_age_ranges = listOf(0,2), actor_info_2_gender_roles = listOf("male"), off_stage_roles_general = listOf("peter pan"), off_stage_roles_production = listOf("producer"),off_stage_roles_scenic = listOf("stage-hand"), off_stage_roles_lighting = listOf("light manger 1", "light manager 1"), off_stage_roles_hair_makeup_costumes = listOf("cosmetologist", "lead cosmetologist"), off_stage_roles_sound = listOf("sound tech 1"), profile_photo_url = "www.awsPhotoURL.com", demographic_union_status = "Pedro Test", demographic_websites = listOf("www.myPersonalProfile.com"))
     private val userProfileNullUnionStatus = ProfileRegistrationDto(pronouns = "he/him", lgbtqplus_member = false, gender_identity = "male", comfortable_playing_transition = true, comfortable_playing_man = true, comfortable_playing_women = true, comfortable_playing_neither = false, height_inches = 88, agency = "Pedro LLC", website_link_one = "", website_link_two = "", website_type_one = "", website_type_two = "", bio = "this is my bio", landing_perform_type_on_stage = true, landing_perform_type_off_stage = false, actor_info_1_ethnicities = listOf("Hispanic"), actor_info_2_age_ranges = listOf(0,2), actor_info_2_gender_roles = listOf("male"), off_stage_roles_general = listOf("peter pan"), off_stage_roles_production = listOf("producer"),off_stage_roles_scenic = listOf("stage-hand"), off_stage_roles_lighting = listOf("light manger 1", "light manager 1"), off_stage_roles_hair_makeup_costumes = listOf("cosmetologist", "lead cosmetologist"), off_stage_roles_sound = listOf("sound tech 1"), profile_photo_url = "www.awsPhotoURL.com", demographic_union_status = null, demographic_websites = listOf("www.myPersonalProfile.com"))
-
+    private val validProfileExtraInfo = ProfileRegistrationExtraInfoDto(null,past_performance = null,upcoming = null, additional_skills_checkboxes = null, additional_skills_manual = null , awards = listOf(AwardRegistrationEntity(name = "Award 1",year_received = 2021, award_url = "testAward1", description = "test description for award")))
     private val validAuthKey = "mockAuthKey"
 
     @Test
@@ -94,6 +93,7 @@ class ProfileControllerIntegrationTests {
         assertEquals(validRegisterProfile.website_type_two, createdProfile.website_type_two)
         assertEquals(validRegisterProfile.bio, createdProfile.bio)
 
+        testDataCreatorService.deleteUser(userIdUUID!!)
     }
 
     @Test
@@ -129,6 +129,7 @@ class ProfileControllerIntegrationTests {
         assertEquals(errorDetailsResponse?.body?.restErrorMessage, RestErrorMessages.UNAUTHORIZED_MESSAGE)
         assertEquals(errorDetailsResponse?.body?.detailedMessage, DetailedErrorMessages.WRONG_AUTH_KEY)
 
+        testDataCreatorService.deleteUser(userIdUUID!!)
     }
 
     @Test
@@ -149,7 +150,6 @@ class ProfileControllerIntegrationTests {
         assertNotNull(errorDetailsResponse?.body?.time)
         assertEquals(errorDetailsResponse?.body?.restErrorMessage, RestErrorMessages.BAD_REQUEST_MESSAGE)
         assertEquals(errorDetailsResponse?.body?.detailedMessage, DetailedErrorMessages.INVALID_USER_ID)
-
     }
 
     @Test
@@ -192,6 +192,8 @@ class ProfileControllerIntegrationTests {
         assertNotNull(errorDetailsResponse?.body?.time)
         assertEquals(errorDetailsResponse?.body?.restErrorMessage, RestErrorMessages.CONFLICT_MESSAGE)
         assertEquals(errorDetailsResponse?.body?.detailedMessage, DetailedErrorMessages.USER_HAS_PROFILE)
+
+        testDataCreatorService.deleteUser(userIdUUID!!)
     }
 
     @Test
@@ -250,6 +252,8 @@ class ProfileControllerIntegrationTests {
         assertEquals(validRegisterProfile.website_type_one, getProfile.website_type_one)
         assertEquals(validRegisterProfile.website_type_two, getProfile.website_type_two)
         assertEquals(validRegisterProfile.bio, getProfile.bio)
+
+        testDataCreatorService.deleteUser(userIdUUID!!)
     }
 
     @Test
@@ -306,6 +310,8 @@ class ProfileControllerIntegrationTests {
         assertNotNull(errorDetailsResponse?.body?.time)
         assertEquals(errorDetailsResponse?.body?.restErrorMessage, RestErrorMessages.NOT_FOUND_MESSAGE)
         assertEquals(errorDetailsResponse?.body?.detailedMessage, DetailedErrorMessages.PROFILE_NOT_FOUND)
+
+        testDataCreatorService.deleteUser(userIdUUID!!)
     }
 
     @Test
@@ -343,6 +349,8 @@ class ProfileControllerIntegrationTests {
         assertNotNull(errorDetailsResponse?.body?.time)
         assertEquals(errorDetailsResponse?.body?.restErrorMessage, RestErrorMessages.BAD_REQUEST_MESSAGE)
         assertEquals(errorDetailsResponse?.body?.detailedMessage, DetailedErrorMessages.UNION_STATUS_NOT_SUPPORTED)
+
+        testDataCreatorService.deleteUser(userIdUUID!!)
     }
 
     @Test
@@ -380,6 +388,90 @@ class ProfileControllerIntegrationTests {
         assertNotNull(errorDetailsResponse?.body?.time)
         assertEquals(errorDetailsResponse?.body?.restErrorMessage, RestErrorMessages.BAD_REQUEST_MESSAGE)
         assertEquals(errorDetailsResponse?.body?.detailedMessage, DetailedErrorMessages.UNION_STATUS_MEMBER_REQUIRED)
+
+        testDataCreatorService.deleteUser(userIdUUID!!)
     }
 
+    @Test
+    fun registerProfileExtraInfo_validInput_201Success() {
+        //create user headers
+        val validRegisterUser = testDataCreatorService.createValidRegisterUser()
+        val headers = HttpHeaders()
+        headers.set("authKey", validAuthKey)
+        val request = HttpEntity(validRegisterUser, headers)
+
+        //create user
+        val createdUserResponse = testRestTemplate.postForEntity("/user/register", request, String::class.java)
+        val createUser = objectMapper.readValue(createdUserResponse.body, UserDto::class.java)
+        val userIdUUID = createUser.userId
+
+        //register profile headers
+        val headers2 = HttpHeaders()
+        headers2.set("authKey", validAuthKey)
+        headers2.set("userId", userIdUUID.toString())
+        val request2 = HttpEntity(validRegisterProfile, headers2)
+
+        //create profile
+        val userProfileResponse = testRestTemplate.exchange("/user/${userIdUUID.toString()}/profile/register", HttpMethod.POST, request2, String::class.java)
+        val createdProfile = objectMapper.readValue(userProfileResponse.body, ProfileDto::class.java)
+
+        //register profile extra info headers
+        val headers3 = HttpHeaders()
+        headers3.set("authKey", validAuthKey)
+        headers3.set("userId", userIdUUID.toString())
+        val request3 = HttpEntity(validProfileExtraInfo, headers3)
+
+        //create profile extra info
+        val userProfileExtraInfoResponse = testRestTemplate.exchange("/user/${userIdUUID.toString()}/profile/register/registertwo", HttpMethod.POST, request3, String::class.java)
+        val createdProfileExtraInfo = objectMapper.readValue(userProfileExtraInfoResponse.body, ProfileExtraInfoDto::class.java)
+
+        //check the created user
+        assertNotNull(createdUserResponse)
+        assertEquals(HttpStatus.CREATED, createdUserResponse.statusCode)
+        assertEquals(validRegisterUser.first_name, createUser.first_name)
+        assertEquals(validRegisterUser.last_name, createUser.last_name)
+        assertEquals(validRegisterUser.email, createUser.email)
+        assertEquals(validRegisterUser.agreed_18, createUser.agreed_18)
+        assertEquals(validRegisterUser.agreed_privacy, createUser.agreed_privacy)
+        assertEquals(true, createUser.active_status)
+        assertNotNull(createUser.userId)
+        assertNotNull(createUser.session_id)
+
+        //check the created profile
+        assertNotNull(userProfileResponse)
+        assertEquals(HttpStatus.CREATED, userProfileResponse.statusCode)
+        assertNotNull(createdProfile.profile_id)
+        assertEquals(validRegisterProfile.pronouns, createdProfile.pronouns)
+        assertEquals(validRegisterProfile.lgbtqplus_member, createdProfile.lgbtqplus_member)
+        assertEquals(validRegisterProfile.gender_identity, createdProfile.gender_identity)
+        assertEquals(validRegisterProfile.comfortable_playing_man, createdProfile.comfortable_playing_man)
+        assertEquals(validRegisterProfile.comfortable_playing_women, createdProfile.comfortable_playing_women)
+        assertEquals(validRegisterProfile.comfortable_playing_neither, createdProfile.comfortable_playing_neither)
+        assertEquals(validRegisterProfile.comfortable_playing_transition, createdProfile.comfortable_playing_transition)
+        assertEquals(validRegisterProfile.height_inches, createdProfile.height_inches)
+        assertEquals(validRegisterProfile.agency, createdProfile.agency)
+        assertEquals(validRegisterProfile.website_link_one, createdProfile.website_link_one)
+        assertEquals(validRegisterProfile.website_link_two, createdProfile.website_link_two)
+        assertEquals(validRegisterProfile.website_type_one, createdProfile.website_type_one)
+        assertEquals(validRegisterProfile.website_type_two, createdProfile.website_type_two)
+        assertEquals(validRegisterProfile.bio, createdProfile.bio)
+
+        //check the created profile extra info
+        assertNotNull(userProfileExtraInfoResponse)
+        assertEquals(HttpStatus.CREATED, userProfileExtraInfoResponse.statusCode)
+        assertNotNull(createdProfile.profile_id)
+        assertEquals(validProfileExtraInfo.awards!![0].name, createdProfileExtraInfo.awards!![0].name)
+        assertEquals(validProfileExtraInfo.awards!![0].award_url, createdProfileExtraInfo.awards!![0].award_url)
+        assertEquals(validProfileExtraInfo.awards!![0].description, createdProfileExtraInfo.awards!![0].description)
+        assertEquals(validProfileExtraInfo.awards!![0].year_received, createdProfileExtraInfo.awards!![0].year_received)
+        assertNotNull(createdProfileExtraInfo.awards!![0].profileEntity)
+        assertEquals(validProfileExtraInfo.training, createdProfileExtraInfo.training)
+        assertEquals(validProfileExtraInfo.past_performance, createdProfileExtraInfo.past_performance)
+        assertEquals(validProfileExtraInfo.upcoming, createdProfileExtraInfo.upcoming)
+        assertEquals(validProfileExtraInfo.additional_skills_checkboxes, createdProfileExtraInfo.additional_skills_checkboxes)
+        assertEquals(validProfileExtraInfo.additional_skills_manual, createdProfileExtraInfo.additional_skills_manual)
+
+        //delete user and cascade
+        testDataCreatorService.deleteUser(userIdUUID!!)
+    }
 }
