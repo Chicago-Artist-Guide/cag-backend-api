9cca386f9c83d4008788e684730f937118ebc5c8
Alejandro Amaya <amayaalex19@gmail.com>
2021-10-27 19:10:04 -0500

    created add_skills_manual.sql

---
+++ src/main/resources/db/migration/V20211027185431__add_skills_manual.sql.sql
@@ -0,0 +1,9 @@
+create table addSkillsManual(
+    skills_id uuid default uuid_generate_v4(),
+    profile_id UUID NOT NULL,
+    skills varchar(250),
+
+    primary key (skills_id),
+    foreign key (profile_id) references profile(profile_id) on Delete cascade
+
+);
